e71abbfaeca50472c61591ead1ce36f95bf97d5b
Louis Gagnon <gagnon@iag.uni-stuttgart.de>
2020-09-21 17:33:40 +0200

    add loop option
    adding a checkbox to allow looping the animation by setting self.Run to true until stop is pressed

--- AnimationLib.py
+++ AnimationLib.py
@@ -23,11 +23,12 @@ import libAsm4 as Asm4
     |                  main class                   |
     +-----------------------------------------------+
 """
-class animateVariable():
+class animateVariable( QtGui.QDialog ):
+    "My tool object"
+
 
     def __init__(self):
         super(animateVariable,self).__init__()
-        self.UI = QtGui.QDialog()
         self.drawUI()
 
 
@@ -64,7 +65,7 @@ class animateVariable():
         self.Run = True
 
         # Now we can draw the UI
-        self.UI.show()
+        self.show()
 
         # select the Float variables that are in the "Variables" group
         self.varList.clear()
@@ -100,137 +101,39 @@ class animateVariable():
     +-----------------------------------------------+
     """
     def onRun(self):
-        self.Run = True
         # the selected variable
         varName = self.varList.currentText()
         begin   = self.minValue.value()
         end     = self.maxValue.value()
         step    = self.stepValue.value()
         sleep   = self.sleepValue.value()
+        loop    = self.loopBox.checkState()
         # basic checks
         if varName:
-            # loop indefinitely
-            if self.Loop.isChecked():
-                while self.Run and self.Loop.isChecked():
-                    self.runFwd(varName)
-            # go back-and-forth indefinitely
-            elif self.Pendulum.isChecked():
-                while self.Run and self.Pendulum.isChecked():
-                    self.runFwd(varName)
-                    self.runBwd(varName)
-            else:
-                self.runFwd(varName)
-            '''
-            varValue = begin
             # if we go forwards ...
-            if end>begin and step>0:
-                while varValue <= end and self.Run:
-                    #setattr( self.Variables, varName, varValue )
-                    #App.ActiveDocument.Model.recompute('True')
-                    #Gui.updateGui()
-                    self.setVarValue(varName,varValue)
-                    self.slider.setValue(varValue)
-                    varValue += step
-                    time.sleep(sleep)
-            # ... or backwards
-            elif end<begin and step<0:
-                while varValue >= end and self.Run:
-                    #setattr( self.Variables, varName, varValue )
-                    #App.ActiveDocument.Model.recompute('True')
-                    #Gui.updateGui()
-                    self.setVarValue(varName,varValue)
-                    self.slider.setValue(varValue)
-                    varValue += step
-                    time.sleep(sleep)
-            '''
+            while self.Run:
+                if end>begin and step>0:
+                    varValue = begin
+                    while varValue <= end and self.Run:
+                        setattr( self.Variables, varName, varValue )
+                        #App.ActiveDocument.recompute()
+                        App.ActiveDocument.Model.recompute('True')
+                        Gui.updateGui()
+                        varValue += step
+                        time.sleep(sleep)
+                # ... or backwards
+                elif end<begin and step<0:
+                    varValue = begin
+                    while varValue >= end and self.Run:
+                        setattr( self.Variables, varName, varValue )
+                        #App.ActiveDocument.recompute()
+                        App.ActiveDocument.Model.recompute('True')
+                        Gui.updateGui()
+                        varValue += step
+                        time.sleep(sleep)
+                if not loop: self.Run = False
+        self.Run = True
         return
-
-
-    def runFwd( self, varName ):
-        begin   = self.minValue.value()
-        end     = self.maxValue.value()
-        step    = self.stepValue.value()
-        sleep   = self.sleepValue.value()
-        varValue = begin
-        # if we go positive ...
-        if end>begin and step>0:
-            while varValue <= end and self.Run:
-                self.setVarValue(varName,varValue)
-                self.slider.setValue(varValue)
-                varValue += step
-                time.sleep(sleep)
-        # ... or negative
-        elif end<begin and step<0:
-            while varValue >= end and self.Run:
-                self.setVarValue(varName,varValue)
-                self.slider.setValue(varValue)
-                varValue += step
-                time.sleep(sleep)
-
-    
-    def runBwd( self, varName ):
-        begin   = self.minValue.value()
-        end     = self.maxValue.value()
-        step    = self.stepValue.value()
-        sleep   = self.sleepValue.value()
-        varValue = end
-        # if we went positive ...
-        if end>begin and step>0:
-            while varValue >= begin and self.Run:
-                self.setVarValue(varName,varValue)
-                self.slider.setValue(varValue)
-                varValue -= step
-                time.sleep(sleep)
-        # ... or negative
-        elif end<begin and step<0:
-            while varValue <= begin and self.Run:
-                self.setVarValue(varName,varValue)
-                self.slider.setValue(varValue)
-                varValue -= step
-                time.sleep(sleep)
-
-
-    def onLoop(self):
-        if self.Pendulum.isChecked() and self.Loop.isChecked():
-            self.Pendulum.setChecked(False)
-        return
-
-
-    def onPendulum(self):
-        if self.Loop.isChecked() and self.Pendulum.isChecked():
-            self.Loop.setChecked(False)
-        return
-
-
-    def setVarValue(self,name,value):
-        setattr( self.Variables, name, value )
-        App.ActiveDocument.Model.recompute('True')
-        Gui.updateGui()
-
-
-    """
-    +-----------------------------------------------+
-    |                   Slider                      |
-    +-----------------------------------------------+
-    """
-    def sliderMoved(self):
-        self.Run = False
-        varName = self.varList.currentText()
-        varValue = self.slider.value()
-        self.setVarValue(varName,varValue)
-        #setattr( self.Variables, varName, varValue )
-        #App.ActiveDocument.Model.recompute('True')
-        #Gui.updateGui()
-        return
-
-
-    def onValuesChanged(self):
-        self.sliderMinValue.setText( str(self.minValue.value()) )
-        self.sliderMaxValue.setText( str(self.maxValue.value()) )
-        self.slider.setRange( self.minValue.value(), self.maxValue.value() )
-        self.slider.setSingleStep( self.stepValue.value() )
-        return
-
 
 
     """
@@ -250,7 +153,7 @@ class animateVariable():
     """
     def onClose(self):
         self.Run = False
-        self.UI.close()
+        self.close()
 
 
     """
@@ -261,101 +164,70 @@ class animateVariable():
     def drawUI(self):
         # Our main window will be a QDialog
         # make this dialog stay above the others, always visible
-        self.UI.setWindowFlags( QtCore.Qt.WindowStaysOnTopHint )
-        self.UI.setWindowTitle('Animate Assembly')
-        self.UI.setWindowIcon( QtGui.QIcon( os.path.join( Asm4.iconPath , 'FreeCad.svg' ) ) )
-        self.UI.setMinimumWidth(470)
-        self.UI.setModal(False)
+        self.setWindowFlags( QtCore.Qt.WindowStaysOnTopHint )
+        self.setWindowTitle('Animate Assembly')
+        self.setWindowIcon( QtGui.QIcon( os.path.join( Asm4.iconPath , 'FreeCad.svg' ) ) )
+        self.setMinimumSize(470, 300)
+        self.resize(470,300)
+        self.setModal(False)
         # set main window widgets
-        self.mainLayout = QtGui.QVBoxLayout(self.UI)
+        self.mainLayout = QtGui.QVBoxLayout(self)
 
         # Define the fields for the form ( label + widget )
-        self.formLayout = QtGui.QFormLayout()
+        self.formLayout = QtGui.QFormLayout(self)
         # select Variable
-        self.varList = QtGui.QComboBox()
+        self.varList = QtGui.QComboBox(self)
         self.formLayout.addRow(QtGui.QLabel('Select Variable'),self.varList)
         # Range Minimum
-        self.minValue = QtGui.QDoubleSpinBox()
+        self.minValue = QtGui.QDoubleSpinBox(self)
         self.minValue.setRange( -1000000.0, 1000000.0 )
         self.formLayout.addRow(QtGui.QLabel('Range Begin'),self.minValue)
         # Maximum
-        self.maxValue = QtGui.QDoubleSpinBox()
+        self.maxValue = QtGui.QDoubleSpinBox(self)
         self.maxValue.setRange( -1000000.0, 1000000.0 )
         self.formLayout.addRow(QtGui.QLabel('Range End'),self.maxValue)
         # Step
-        self.stepValue = QtGui.QDoubleSpinBox()
+        self.stepValue = QtGui.QDoubleSpinBox(self)
         self.stepValue.setRange( -10000.0, 10000.0 )
         self.stepValue.setValue( 1.0 )
         self.formLayout.addRow(QtGui.QLabel('Step'),self.stepValue)
         # Sleep
-        self.sleepValue = QtGui.QDoubleSpinBox()
+        self.sleepValue = QtGui.QDoubleSpinBox(self)
         self.sleepValue.setRange( 0.0, 10.0 )
         self.sleepValue.setValue( 0.0 )
         self.formLayout.addRow(QtGui.QLabel('Sleep (s)'),self.sleepValue)
         # apply the layout
         self.mainLayout.addLayout(self.formLayout)
-        self.mainLayout.addWidget(QtGui.QLabel())
-
-        # slider
-        self.sliderLayout = QtGui.QHBoxLayout()
-        self.slider = QtGui.QSlider()
-        self.slider.setOrientation(QtCore.Qt.Orientation.Horizontal)
-        self.slider.setRange(0, 10)
-        self.slider.setTickInterval(0)
-        self.sliderMinValue = QtGui.QLabel('Min')
-        self.sliderMaxValue = QtGui.QLabel('Max')
-        self.sliderLayout.addWidget(self.sliderMinValue)
-        self.sliderLayout.addWidget(self.slider)
-        self.sliderLayout.addWidget(self.sliderMaxValue)
-        self.mainLayout.addLayout(self.sliderLayout)
-        
-        # loop and pendumlum tick-boxes
-        self.Loop = QtGui.QCheckBox()
-        self.Loop.setLayoutDirection(QtCore.Qt.RightToLeft)
-        self.Loop.setToolTip("Infinite Loop")
-        self.Loop.setText("Loop")
-        self.Loop.setChecked(False)
-        self.mainLayout.addWidget(self.Loop)
-        self.Pendulum = QtGui.QCheckBox()
-        self.Pendulum.setLayoutDirection(QtCore.Qt.RightToLeft)
-        self.Pendulum.setToolTip("Back-and-forth pendulum")
-        self.Pendulum.setText("Pendulum")
-        self.Pendulum.setChecked(False)
-        self.mainLayout.addWidget(self.Pendulum)
-
-        self.mainLayout.addWidget(QtGui.QLabel())
         self.mainLayout.addStretch()
+
         # the button row definition
-        self.buttonLayout = QtGui.QHBoxLayout()
+        self.buttonLayout = QtGui.QHBoxLayout(self)
         # Close button
-        self.CloseButton = QtGui.QPushButton('Close')
+        self.CloseButton = QtGui.QPushButton('Close', self)
         self.buttonLayout.addWidget(self.CloseButton)
         self.buttonLayout.addStretch()
         # Stop button
-        self.StopButton = QtGui.QPushButton('Stop')
+        self.StopButton = QtGui.QPushButton('Stop', self)
         self.buttonLayout.addWidget(self.StopButton)
         self.buttonLayout.addStretch()
         # Run button
-        self.RunButton = QtGui.QPushButton('Run')
+        self.RunButton = QtGui.QPushButton('Run', self)
         self.RunButton.setDefault(True)
         self.buttonLayout.addWidget(self.RunButton)
+        # Loop checkbox
+        self.loopBox = QtGui.QCheckBox("Loop", self)
+        self.buttonLayout.addWidget(self.loopBox)
         # add buttons to layout
         self.mainLayout.addLayout(self.buttonLayout)
 
         # finally, apply the layout to the main window
-        self.UI.setLayout(self.mainLayout)
+        self.setLayout(self.mainLayout)
 
         # Actions
         self.varList.currentIndexChanged.connect( self.onSelectVar )
-        self.slider.sliderMoved.connect(          self.sliderMoved)
-        self.minValue.valueChanged.connect(       self.onValuesChanged )
-        self.maxValue.valueChanged.connect(       self.onValuesChanged )
-        self.stepValue.valueChanged.connect(      self.onValuesChanged )
-        self.Loop.toggled.connect(                self.onLoop )
-        self.Pendulum.toggled.connect(            self.onPendulum )
-        self.CloseButton.clicked.connect(         self.onClose )
-        self.StopButton.clicked.connect(          self.onStop )
-        self.RunButton.clicked.connect(           self.onRun )
+        self.CloseButton.clicked.connect(self.onClose)
+        self.StopButton.clicked.connect(self.onStop)
+        self.RunButton.clicked.connect(self.onRun)
 
 
 
